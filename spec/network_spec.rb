e42a10c22526fa5e530172cd73639e42425edb44
Miriam Aceves <miriammerari02@gmail.com>
2021-05-26 13:05:00 -0400

    Create network class and spec file

---
+++ spec/network_spec.rb
@@ -0,0 +1,11 @@
+require './lib/doctor'
+require './lib/hospital'
+require './lib/network'
+
+RSpec.describe Network do
+  it "has a name" do
+    gsmn = Network.new("Greater Seattle Medical Network")
+
+    expect(gsmn.name).to eq("Greater Seattle Medical Network")
+  end
+end
